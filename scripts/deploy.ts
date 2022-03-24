0360b5b2b557913c2e1ae7308e8b2fc248d924b9
GooGrand <sharkanovnikita@gmail.com>
2022-03-24 20:26:13 +0600

    Upd: rewrite deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,34 @@
 import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 async function main() {
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const GCD = await ethers.getContractFactory("GCD");
+  const BondNFT = await ethers.getContractFactory("BondNFT");
+  const Minter = await ethers.getContractFactory("Minter");
 
-  await greeter.deployed();
+  const gcd = await GCD.deploy("GTON USD", "GCD");
+  await gcd.deployed();
+  const bond = await BondNFT.deploy("GCD Bond", "GCDB");
+  await bond.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  const minter = await Minter.deploy(
+    process.env.GTON_ADDRESS || "",
+    gcd.address,
+    bond.address,
+    process.env.ORACLE_PRICE || "",
+    process.env.POOL_ADDRESS || "",
+    process.env.DISCOUNT || ""
+  );
+
+  await minter.deployed();
+
+  await bond.transferOwnership(minter.address);
+  await gcd.transferOwnership(minter.address);
+
+  console.log("GCD deployed to:", gcd.address);
+  console.log("Bond deployed to:", bond.address);
+  console.log("Minter deployed to:", minter.address);
 }
 
 main().catch((error) => {
