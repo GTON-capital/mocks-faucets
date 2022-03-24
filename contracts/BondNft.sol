f276a24b22ae810676e7d948e5f2b5b838dc44d9
GooGrand <sharkanovnikita@gmail.com>
2022-03-24 20:10:41 +0600

    Upd: finish contract logic

--- contracts/BondNft.sol
+++ contracts/BondNft.sol
@@ -1,10 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { IBondNFT } from "./interfaces/IBondNFT.sol";
+
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BondNFT is ERC721, Ownable {
+contract BondNFT is IBondNFT, ERC721, Ownable {
+    using Counters for Counters.Counter;
 
-    
+    Counters.Counter private _tokenIdTracker;
+    // Probably it is possible to use uint[] here, but if so - fix Minter.sol 51 line
+    mapping(uint => uint) public bondAmount;
+
+    constructor(string memory name, string memory symbol) 
+        ERC721(name, symbol) {}
+
+    function mint(address to, uint amount) external override onlyOwner returns(uint id) {
+        id = _tokenIdTracker.current();
+        bondAmount[id] = amount;
+        _mint(to, id);
+        _tokenIdTracker.increment();
+    }
 }
--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -5,43 +5,67 @@ import "hardhat/console.sol";
 
 import { IMinter } from "./interfaces/IMinter.sol";
 import { IGCD } from "./interfaces/IGCD.sol";
+import { IPMM } from "./interfaces/IPMM.sol";
+import { BondNFT } from "./BondNFT.sol";
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
 contract Minter is IMinter, ERC721Holder {
 
-    constructor(ERC20 _gton, IGCD _gcd, ERC721 _bond, AggregatorV3Interface _price) {
+    constructor(
+        ERC20 _gton, 
+        IGCD _gcd, 
+        BondNFT _bond, 
+        AggregatorV3Interface _price, 
+        IPMM _pool,
+        uint _discountNominator
+        ) {
         bond = _bond;
         gton = _gton;
         gcd = _gcd;
         gtonPrice = _price;
+        pool = _pool;
+        discountNominator = _discountNominator;
     }
 
     // ---------- Variables -----------
-
+    uint public discountNominator;
     AggregatorV3Interface public gtonPrice;
-
+    IPMM public pool;
 
     // ---------- Constants -----------
-    ERC721 immutable public bond;
+    uint immutable public denominator = 10000;
+    BondNFT immutable public bond;
     ERC20 immutable public gton;
     IGCD immutable public gcd;
 
-    function mint(uint amount) public override returns(uint id) {
+    // ---------- Views ---------------
+
+    function amountWithoutDiscount(uint amount) public view returns(uint) {
+        uint givenPercent = denominator - discountNominator;
+        return amount * denominator / givenPercent;
+    }
+
+    // ---------- Methods -------------
+
+    function mint(uint amount) external override returns(uint id) {
         gton.transferFrom(msg.sender, address(this), amount);
         (,int256 price,,,) = gtonPrice.latestRoundData();
         uint8 decimals = gtonPrice.decimals();
-        uint mintAmount = amount * uint(price) / decimals;
-        gcd.mint(address(this), mintAmount);
-
-        // TODO put gcd + gton to PMM
-        // TODO issue bond
+        uint mintAmount = amountWithoutDiscount(amount) * uint(price) / decimals;
+        gcd.mint(address(this), mintAmount * 2);
+        pool.updateAsk(amount); // gton
+        pool.updateBid(mintAmount); // gcd
+        id = bond.mint(msg.sender, mintAmount);
     }
 
-    function claim(uint bondId) public override {
-
+    function claim(uint bondId) external override {
+        // After transfer bond stays on the contract and can't be transfered etc, so we are safe here
+        // And this transfer prevents from passing id that you do not own
+        bond.safeTransferFrom(msg.sender, address(this), bondId);
+        uint amount = bond.bondAmount(bondId);
+        gcd.transfer(msg.sender, amount);
     }
 }
